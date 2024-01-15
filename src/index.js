d555f5ccdeedb75014d4e8f0d6a941820b29fd38
sunil <iamsunilsharma999@gmail.com>
2024-01-15 10:39:27 +0530

    implementation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+const isDev = (ifTrue = true, ifFalse = false) =>
+  !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+    ? ifTrue
+    : ifFalse;
+
+export default isDev;
